facbf47af792f62a4605895637701fd97464c033
Jacob Moreno <jacob.w.moreno@gmail.com>
2021-03-03 10:59:54 -0700

    Init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Title from './comps/Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+  video: https://www.youtube.com/watch?v=vUe91uOx7R0
+  time: 9:00
+*/
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2LbQSP8oF5QaibFPBWtpIDjZh8W9ICI0",
+  authDomain: "firegram-53615.firebaseapp.com",
+  projectId: "firegram-53615",
+  storageBucket: "firegram-53615.appspot.com",
+  messagingSenderId: "553023231073",
+  appId: "1:553023231073:web:b689b664515d5bdfe10620"
+};
+
+// Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig);
